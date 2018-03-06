c6f70eb58c75362478c022efbca3485596fe697f
autumnste <autumnste@live.com>
2018-03-06 18:18:40 +0800

    基本界面框架搭建

--- MeiTuan/MeiTuan/firstViewController.h
+++ MeiTuan/MeiTuan/firstViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  firstViewController.h
 //  MeiTuan
 //
 //  Created by WangXinjia on 2018/3/6.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface firstViewController : UIViewController
 
 @end
-
