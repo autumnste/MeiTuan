83298ba2a9ad00dd91da44ecdd95a2b440232d37
autumnste <autumnste@live.com>
2018-03-07 19:23:16 +0800

    初步搭建mine界面

---
+++ MeiTuan/MeiTuan/profileView.h
@@ -0,0 +1,13 @@
+//
+//  profileView.h
+//  MeiTuan
+//
+//  Created by WangXinjia on 2018/3/7.
+//  Copyright © 2018年 WangXinjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface profileView : UIView
+
+@end
---
+++ MeiTuan/MeiTuan/profileView.m
@@ -0,0 +1,21 @@
+//
+//  profileView.m
+//  MeiTuan
+//
+//  Created by WangXinjia on 2018/3/7.
+//  Copyright © 2018年 WangXinjia. All rights reserved.
+//
+
+#import "profileView.h"
+
+@implementation profileView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
