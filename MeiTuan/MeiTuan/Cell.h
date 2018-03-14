8ca69a5f0a0fedaa4867358eb1068d6e30dffe5e
autumnste <autumnste@live.com>
2018-03-14 17:28:24 +0800

    添加Sqlite数据库，displayCell开始搭建

---
+++ MeiTuan/MeiTuan/Cell.h
@@ -0,0 +1,13 @@
+//
+//  Cell.h
+//  MeiTuan
+//
+//  Created by WangXinjia on 2018/3/14.
+//  Copyright © 2018年 WangXinjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Cell : UITableViewCell
+
+@end
---
+++ MeiTuan/MeiTuan/Cell.m
@@ -0,0 +1,24 @@
+//
+//  Cell.m
+//  MeiTuan
+//
+//  Created by WangXinjia on 2018/3/14.
+//  Copyright © 2018年 WangXinjia. All rights reserved.
+//
+
+#import "Cell.h"
+
+@implementation Cell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
